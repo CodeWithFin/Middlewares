357411e72dfa56324bc34ab5b91d70a92126bfcf
Finley <finley.mwachia12@gmail.com>
2024-06-01 12:39:55 +0300

    completed assignment

--- index1.js
+++ index1.js
@@ -1,10 +1,17 @@
 import express from "express";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.post("/submit",(req,res)=>{
+  console.log(req.body);
+});
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
