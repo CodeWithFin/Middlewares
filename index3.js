a82d69dec9625080a4c7089aa1771b103e0daa21
Finley <finley.mwachia12@gmail.com>
2024-06-02 08:42:38 +0300

    added the post request and get request to index4.js file

--- index3.js
+++ index3.js
@@ -6,7 +6,7 @@ const port = 3000;
 function logger(req,res,next){
 console.log("Request Method: ",req.method);
 console.log("Request URL: ",req.url);
-next()
+next();
 }
 
 app.use(logger);
--- index4.js
+++ index4.js
@@ -1,8 +1,25 @@
 import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({extended:true}));
+app.post("/submit",(req,res)=>{
+console.log(req.body)
+});
+
+app.get("/",(req,res)=>{
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
