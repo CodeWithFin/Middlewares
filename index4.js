9fd6bd7636504d310f0e3fe84ff91414092f293c
Finley <finley.mwachia12@gmail.com>
2024-06-04 09:03:21 +0300

    done with the challenge

--- index4.js
+++ index4.js
@@ -3,21 +3,32 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import bodyParser from "body-parser";
 
+
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
+var bandName = "";
 
 app.use(bodyParser.urlencoded({extended:true}));
-app.post("/submit",(req,res)=>{
-console.log(req.body);
-});
+
+
+function bandNameGenerator(req,res,next){
+ console.log(req.body);
+ bandName = req.body["street"]+req.body["pet"];
+ next();
+}
+app.use(bandNameGenerator);
+
 
 app.get("/",(req,res)=>{
   res.sendFile(__dirname + "/public/index.html");
+  
 });
 
-
+app.post("/submit", (req, res) => {
+  res.send(`<h1>Your band name is:</h1><h2>${bandName}✌️</h2>`);
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
